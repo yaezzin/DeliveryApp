4e36ccf411567a5b50deedd82d163efa3fd3de0d
Ivan Kim <xormrdlsrks2@gmail.com>
2023-10-05 22:47:29 +0900

    [REV] sajjang models.py >> required instances have been added

--- app/sajjang/models.py
+++ app/sajjang/models.py
@@ -9,15 +9,8 @@ from account.models import Address
 class Category(models.Model):
     name = models.CharField(max_length=20, verbose_name="name")
 
-
-class Menus(models.Model):
-    category_id = models.ForeignKey(
-        Category, on_delete=models.CASCADE, verbose_name="category_id"
-    )
-    name = models.CharField(max_length=128)
-    unit_price = models.IntegerField()
-    menu_pic = models.ImageField()
-    is_available = models.BooleanField()
+    def __str__(self) -> str:
+        return self.name
 
 
 class Stores(models.Model):
@@ -32,6 +25,22 @@ class Stores(models.Model):
         return self.name
 
 
+class Menus(models.Model):
+    store_id = models.ForeignKey(
+        Stores, on_delete=models.CASCADE, verbose_name="store_id"
+    )
+    category_id = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="category_id"
+    )
+    name = models.CharField(max_length=128)
+    unit_price = models.IntegerField()
+    menu_pic = models.ImageField()
+    is_available = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Order(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="user_id")
     stores_id = models.ForeignKey(
@@ -43,7 +52,9 @@ class Order(models.Model):
     total_price = models.IntegerField(verbose_name="total_price")
     create_time = models.DateTimeField(auto_now_add=True)
     paid_status = models.BooleanField(default=False, verbose_name="paid_status")
-    delivery_status = models.BooleanField(default=False, verbose_name="delivery_status")
+    delivery_status = models.BooleanField(
+        null=True, default=None, verbose_name="delivery_status"
+    )
     is_sajjang_accepted = models.BooleanField(
         default=False, verbose_name="is_sajjang_accepted"
     )
