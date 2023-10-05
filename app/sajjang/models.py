667da25ba0727e5f0cef76cd3643067122fc2226
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-10-05 10:28:13 +0900

    [ADD] : sajjang models.py meuns 작성

--- app/sajjang/models.py
+++ app/sajjang/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Menus(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='category_id')
+    name = models.CharField(max_length=128)
+    unit_price = models.IntegerField()
+    menu_pic = models.ImageField()
+    is_available = models.BooleanField()
