141d7d4ce71af88f88a192638204fbf9f1dd07b4
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-10-05 13:02:29 +0900

    [ADD] customer/models.py >> cart model 작성

--- app/customer/models.py
+++ app/customer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from sajjang.models import Menus, Order
+
+from store.models import Store
 
 # Create your models here.
+class Cart(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user_id')
+    stores_id = models.ForeignKey(Store, on_delete=models.CASCADE, verbose_name='stores_id')
+    menus_id = models.ForeignKey(Menus, on_delete=models.CASCADE, verbose_name='menus_id')
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='order_id')
+    quantity = models.IntegerField()
+    unit_price = models.IntegerField()
+    create_time = models.DateTimeField(auto_now_add=True)
