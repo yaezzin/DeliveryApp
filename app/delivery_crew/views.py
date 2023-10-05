b51d41e204283e7c62c396dd76926418c91531c8
Hansol Lee <gksthf996@naver.com>
2023-10-05 16:25:53 +0900

    [ADD] view 모델 작성 >> delivery_crew view 모델 뼈대 작성

--- app/delivery_crew/views.py
+++ app/delivery_crew/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class DeliveryCrewHomeView(TemplateView):
+    def get(self, request):
+        pass
+
+
+class DeliveryCrewAcceptView(TemplateView):
+    def get(self, request, order_id):
+        pass
+
+
+class DeliveryCrewDenyView(TemplateView):
+    def get(self, request, order_id):
+        pass
+
+
+class DeliveryCrewAlarmView(TemplateView):
+    def get(self, request, user_id):
+        pass
