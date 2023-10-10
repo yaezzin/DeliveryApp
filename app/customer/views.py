da8a851d1fe53d50fd16cd8d1284d76108bed8ab
Ivan Kim <xormrdlsrks2@gmail.com>
2023-10-10 13:40:26 +0900

    [REV] update lint, fix typo

--- app/customer/views.py
+++ app/customer/views.py
@@ -22,7 +22,7 @@ class CustomerHomeView(TemplateView):
         else:
             stores = Stores.objects.all()
 
-        search_query = request.GET.get('search', None)
+        search_query = request.GET.get("search", None)
         if search_query:
             stores = Stores.objects.filter(name__contains=search_query)
 
@@ -115,17 +115,21 @@ class CustomerAddressDeleteView(TemplateView):
 
 class CustomerCartView(TemplateView):
     template_name = "/app/customer/templates/cart/list.html"
+
     def get(self, request):
         user_carts = Cart.objects.filter(user_id=request.user.pk, order_id=None)
-        stores = user_carts.distinct().values_list('store_id')
-        context = {'carts':[]}
+        stores = user_carts.distinct().values_list("store_id")
+        context = {"carts": []}
         for store in stores:
             store_name = Stores.objects.filter(id=store[0]).first().name
-            
-            context['carts'].append({'store_name':store_name, 
-                                     'carts':user_carts.filter(store_id=store[0])}
-                                     )
-        
+
+            context["carts"].append(
+                {
+                    "store_name": store_name,
+                    "carts": user_carts.filter(store_id=store[0]),
+                }
+            )
+
         print(context)
         return render(request, self.template_name, context)
 
@@ -145,27 +149,30 @@ class CustomerOrderCreateView(TemplateView):
     def post(self, request, id):
         pass
 
+
 # /customer/store/
 class CustomerStoreView(TemplateView):
-    template_name = '/app/customer/templates/store/search.html'
+    template_name = "/app/customer/templates/store/search.html"
 
     def get(self, request):
         stores = Stores.objects.filter(status=True)
-        context = {"stores" : stores}
+        context = {"stores": stores}
         return render(request, self.template_name, context)
+
 
 # /customer/store/<int:store_id>
 class CustomerStoreDetailView(TemplateView):
-    template_name = '/app/customer/templates/store/detail.html'
+    template_name = "/app/customer/templates/store/detail.html"
 
     def get(self, request, store_id):
         store = get_object_or_404(Stores, id=store_id)
-        context = {"store": store}      
+        context = {"store": store}
         return render(request, self.template_name, context)
+
 
 # /customer/store/<int:store_id>/menu/
 class CustomerStoreMenuView(TemplateView):
-    template_name = '/app/customer/templates/store/menu/list.html'
+    template_name = "/app/customer/templates/store/menu/list.html"
 
     def get(self, request, store_id):
         store = get_object_or_404(Stores, id=store_id)
@@ -173,20 +180,21 @@ class CustomerStoreMenuView(TemplateView):
         context = {"store": store, "menus": menus}
         return render(request, self.template_name, context)
 
+
 # /customer/store/<int:stores_id>/menu/{menus_id}
 class CustomerMenuDetailView(TemplateView):
-    template_name = '/app/customer/templates/store/menu/detail.html'
+    template_name = "/app/customer/templates/store/menu/detail.html"
 
     def get(self, request, store_id, menu_id):
         menus = get_object_or_404(Menus, id=menu_id)
-        
+
         context = {"menus": menus}
         return render(request, self.template_name, context)
 
     def post(self, request, store_id, menu_id):
         try:
-            quantity = request.POST.get('quantity', 1)
-            
+            quantity = request.POST.get("quantity", 1)
+
             store = get_object_or_404(Stores, id=store_id)
             menu = get_object_or_404(Menus, id=menu_id)
 
@@ -195,27 +203,29 @@ class CustomerMenuDetailView(TemplateView):
                 store_id=store,
                 menu_id=menu,
                 order_id=None,
-                quantity=quantity
+                quantity=quantity,
             )
             cart_item.save()
             return redirect("customer:store_menu", store_id=store_id)
-        
+
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=400)
+
 
 # customer/category/
 class CustomerCategoryView(TemplateView):
     template_name = "/app/customer/templates/category/category.html"
-    
+
     def get(self, request):
         categories = Category.objects.all()
         context = {"categories": categories}
         return render(request, self.template_name, context)
 
+
 # customer/category/<int:category_id>
 class CustomerCategoryDetailView(TemplateView):
     template_name = "/app/customer/templates/category/category.html"
-    
+
     def get(self, request, category_id):
         category = Category.objects.filter(id=category_id)
         stores = Stores.objects.filter(category_id=category_id)
@@ -238,57 +248,57 @@ class CustomerCategoryDetailView(TemplateView):
 
 class CustomerOrderDetailView(TemplateView):
     def get(self, request, order_id):
-        template_name = 'orders/detail.html'
+        template_name = "orders/detail.html"
         context = {}
         order = Order.objects.filter(id=order_id)
-        context['order'] = order
-        context['order_id'] = order_id
+        context["order"] = order
+        context["order_id"] = order_id
 
         return render(request, template_name=template_name, context=context)
 
 
 class CustomerPaymentView(TemplateView):
-    
     def get(self, request, order_id):
-        template_name = 'payment/process.html'
+        template_name = "payment/process.html"
         context = {}
-        context['order'] = Order.objects.filter(id=order_id)
+        context["order"] = Order.objects.filter(id=order_id)
         return render(request, template_name=template_name, context=context)
 
     def post(self, request, order_id):
         STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY", "publishable_key")
-        STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "secret_key")        
-        STRIPE_API_VERSION = os.getenv("STRIPE_API_VERSION", 'api_version')
-
+        STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "secret_key")
+        STRIPE_API_VERSION = os.getenv("STRIPE_API_VERSION", "api_version")
 
         stripe.api_key = STRIPE_SECRET_KEY
         stripe.api_version = STRIPE_API_VERSION
 
-        success_url = request.build_absolute_uri('/customer/pay_complete')
-        cancel_url = request.build_absolute_uri('/customer/pay_cancle')
+        success_url = request.build_absolute_uri("/customer/pay_complete")
+        cancel_url = request.build_absolute_uri("/customer/pay_cancle")
 
         session_data = {
-            'mode': 'payment',
-            'client_reference_id': 1,
-            'success_url': success_url,
-            'cancel_url': cancel_url,
-            'line_items': []
+            "mode": "payment",
+            "client_reference_id": 1,
+            "success_url": success_url,
+            "cancel_url": cancel_url,
+            "line_items": [],
         }
 
-        
-        session_data['line_items'].append({
-            'price_data': {
-                'unit_amount': int(15 * Decimal(100)),
-                'currency': 'usd',
-                'product_data': {
-                    'name': '후라이드',
-                }
-            },
-            'quantity': 2,
-        })
+        session_data["line_items"].append(
+            {
+                "price_data": {
+                    "unit_amount": int(15 * Decimal(100)),
+                    "currency": "usd",
+                    "product_data": {
+                        "name": "후라이드",
+                    },
+                },
+                "quantity": 2,
+            }
+        )
 
         checkout_session = stripe.checkout.Session.create(**session_data)
         return redirect(checkout_session.url, code=303)
+
 
 class CustomerPayCompletedView(TemplateView):
     def get(self, request):
