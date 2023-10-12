fc89200c935d52e4a55a5602a185ce23a7839229
Ivan Kim <xormrdlsrks2@gmail.com>
2023-10-12 13:49:05 +0900

    [ADD] mixin function to proceed permission check

--- app/customer/webhook.py
+++ app/customer/webhook.py
@@ -4,30 +4,31 @@ from django.views.decorators.csrf import csrf_exempt
 from sajjang.models import Order
 import os
 
-STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', "webhook_secret")
+STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET", "webhook_secret")
+
 
 @csrf_exempt
 def stripe_webhook(requset):
-    
-
     payload = requset.body
-    sig_header = requset.META['HTTP_STRIPE_SIGNATURE']
+    sig_header = requset.META["HTTP_STRIPE_SIGNATURE"]
     event = None
 
     try:
-        event = stripe.Webhook.construct_event(payload, sig_header, STRIPE_WEBHOOK_SECRET)
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, STRIPE_WEBHOOK_SECRET
+        )
     except ValueError as e:
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
         return HttpResponse(status=400)
-    
-    if event.type == 'checkout.session.completed':
+
+    if event.type == "checkout.session.completed":
         session = event.data.object
-        if session.mode == 'payment' and session.payment_status == 'paid':
+        if session.mode == "payment" and session.payment_status == "paid":
             try:
                 print("Webhook function active")
                 Order.objects.get()
             except Order.DoesNotExist:
                 return HttpResponse(status=404)
-            
+
     return HttpResponse(status=200)
