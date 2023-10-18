71a290a608b3e313f726e15580765ba744b3ea53
Hemin Jeong <jhemin0415@gmail.com>
2023-10-18 17:29:55 +0900

    주소 관련 모델 수정, 카카오 우편번호 검색 API 적용
    - models.py -> [FIX] 주소 입력란 세분화
    - add.html, edit.html -> [FIX] 주소 입력한 변경, 카카오 우편번호 API 적용
    - views.py -> [FIX] post로 받아온 데이터 처리
    - base.html -> 추가 스크립트 작성을 위한 블록 추가

--- app/sajjang/models.py
+++ app/sajjang/models.py
@@ -13,7 +13,11 @@ class Category(models.Model):
 class Stores(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id")
     name = models.CharField(max_length=128)
-    address = models.CharField(max_length=128)
+    address = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=20)
+    base_address = models.CharField(max_length=100)
+    detail_address = models.CharField(max_length=50)
+    extra_address = models.CharField(max_length=100)
     store_pic = models.URLField(null=True, blank=True)
     category_id = models.ForeignKey(
         Category, on_delete=models.CASCADE, db_column="category_id"
